2c92e7624d52f0395cb58c7e15f5e8d27e47e8de
lthihuongn <lilou33127v@gmail.com>
2024-03-12 16:30:38 +0100

    creation stacks and function fill background stacks

--- main.c
+++ main.c
@@ -1,15 +1,49 @@
 #include "include.h"
-
 #include "struct.h"
 
 #define WINDOWTITLE "SDL2 - Projet"
-#define WINDOWWIDTH 1600
-#define WINDOWHEIGHT 800
+#define WINDOWWIDTH 1200
+#define WINDOWHEIGHT 500
 #define FPS 60
+
+#define STACKWIDTH WINDOWWIDTH/3
+#define STACKHEIGHT WINDOWHEIGHT/2
 
 #define POLICE_SIZE 20
 
-int main(void)
+
+// Définition des stacks
+Stack stack1 = {.x = 0, .y = 0, .w = STACKWIDTH, .h = STACKHEIGHT, .r = 0, .g=0, .b=255, .a=255};
+Stack stack2 = {.x = STACKWIDTH, .y = 0, .w = STACKWIDTH, .h = STACKHEIGHT, .r = 0, .g=255, .b=0, .a=255};
+Stack stack3 = {.x = 2 * STACKWIDTH, .y = 0, .w = STACKWIDTH, .h = STACKHEIGHT, .r = 255, .g=0, .b=0, .a=255};
+Stack stack4 = {.x = 0, .y = STACKHEIGHT, .w = STACKWIDTH, .h = STACKHEIGHT, .r = 255, .g=165, .b=0, .a=255};
+Stack stack5 = {.x = STACKWIDTH, .y = STACKHEIGHT, .w = STACKWIDTH, .h = STACKHEIGHT, .r = 128, .g=0, .b=128, .a=255};
+Stack stack6 = {.x = 2 * STACKWIDTH, .y = STACKHEIGHT, .w = STACKWIDTH, .h = STACKHEIGHT, .r = 255, .g=192, .b=203, .a=255};
+
+void fillBackgroundStack(SDL_Renderer *renderer){
+    SDL_SetRenderDrawColor(renderer, stack1.r, stack1.g, stack1.b, stack1.a);
+    SDL_RenderFillRect(renderer, &stack1.rect);
+
+    SDL_SetRenderDrawColor(renderer, stack2.r, stack2.g, stack2.b, stack2.a);
+    SDL_RenderFillRect(renderer, &stack2.rect);
+
+    SDL_SetRenderDrawColor(renderer, stack3.r, stack3.g, stack3.b, stack3.a);
+    SDL_RenderFillRect(renderer, &stack3.rect);
+
+    SDL_SetRenderDrawColor(renderer, stack4.r, stack4.g, stack4.b, stack4.a);
+    SDL_RenderFillRect(renderer, &stack4.rect);
+    
+    SDL_SetRenderDrawColor(renderer, stack5.r, stack5.g, stack5.b, stack5.a);
+    SDL_RenderFillRect(renderer, &stack5.rect);
+
+    SDL_SetRenderDrawColor(renderer, stack6.r, stack6.g, stack6.b, stack6.a);
+    SDL_RenderFillRect(renderer, &stack6.rect);
+    
+}
+
+
+
+int main(int argc, char *argv[])
 {
 #pragma region Initialisation de SDL
     // Initialisation de SDL
@@ -27,16 +61,6 @@ int main(void)
         return -1;
     }
 
-    // Initialisation de SDL_image avec les formats PNG et JPG
-    int imgFlags = IMG_INIT_PNG | IMG_INIT_JPG;
-    if (!(IMG_Init(imgFlags) & imgFlags))
-    {
-        printf("SDL_image n'a pas pu s'initialiser ! IMG_Error: %s\n", IMG_GetError());
-        TTF_Quit();
-        SDL_Quit();
-        return -1;
-    }
-
     // Création de la fenêtre
     SDL_Window *window = SDL_CreateWindow(WINDOWTITLE,
                                           SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
@@ -44,7 +68,6 @@ int main(void)
     if (window == NULL)
     {
         printf("La fenêtre n'a pas pu être créée ! SDL_Error: %s\n", SDL_GetError());
-        IMG_Quit();
         TTF_Quit();
         SDL_Quit();
         return -1;
@@ -56,7 +79,6 @@ int main(void)
     {
         printf("Le renderer n'a pas pu être créé ! SDL_Error: %s\n", SDL_GetError());
         SDL_DestroyWindow(window);
-        IMG_Quit();
         TTF_Quit();
         SDL_Quit();
         return -1;
@@ -69,7 +91,6 @@ int main(void)
         printf("La police n'a pas pu être chargée ! TTF_Error: %s\n", TTF_GetError());
         SDL_DestroyRenderer(renderer);
         SDL_DestroyWindow(window);
-        IMG_Quit();
         TTF_Quit();
         SDL_Quit();
         return -1;
@@ -79,6 +100,16 @@ int main(void)
     SDL_Color textColorWhite = {255, 255, 255, 255}; // White text
     SDL_Color textColorBlack = {0, 0, 0, 255};       // Black text
     SDL_Color textColorGray = {128, 128, 128, 255};  // Gray text
+
+    stack1.rect = (SDL_Rect){stack1.x, stack1.y, stack1.w, stack1.h};
+    stack2.rect = (SDL_Rect){stack2.x, stack2.y, stack2.w, stack2.h};
+    stack3.rect = (SDL_Rect){stack3.x, stack3.y, stack3.w, stack3.h};
+    stack4.rect = (SDL_Rect){stack4.x, stack4.y, stack4.w, stack4.h};
+    stack5.rect = (SDL_Rect){stack5.x, stack5.y, stack5.w, stack5.h};
+    stack6.rect = (SDL_Rect){stack6.x, stack6.y, stack6.w, stack6.h};
+
+
+
 #pragma endregion
     SDL_Event event;
     Bool running = TRUE;
@@ -94,19 +125,22 @@ int main(void)
                 running = FALSE;
                 break;
             }
-            // Nettoyage de l'écran
         }
+
+        // Nettoyage de l'écran
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
         SDL_RenderClear(renderer);
 
-        // Dessin de l'écran
+        // Dessiner la grille
+        fillBackgroundStack(renderer);
+
         SDL_RenderPresent(renderer);
-        SDL_Delay(1000 / FPS);
+        SDL_Delay(1000/FPS);
     }
+
     // Nettoyage et fermeture des bibliothèques et de SDL
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
-    IMG_Quit();
     TTF_Quit();
     SDL_Quit();
     return 0;
--- struct.h
+++ struct.h
@@ -4,11 +4,14 @@ gcc main.c -o main $(sdl2-config --cflags --libs) -lSDL2_ttf -lSDL2_image
 ./main
 
 Windows :
+gcc -std=c17 main.c -IC:\SDL\Projet_C_Compta_SDL\SDL2\x86_64-w64-mingw32\
+include -LC:\SDL\Projet_C_Compta_SDL\SDL2\x86_64-w64-mingw32\lib
+ -Wall -lmingw32 -lSDL2main -lSDL2 -lSDL2_ttf -o main
+
 
 */
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
-#include <SDL2/SDL_image.h>
 
 typedef enum Bool
 {
@@ -36,6 +39,7 @@ typedef struct Stack{
     int quantity;
     int x,y,h,w;
     char text[1024];
+    int r, g, b, a;
     SDL_Surface *surface_text;
     SDL_Texture *texture_text;
     SDL_Rect renderQuad;
