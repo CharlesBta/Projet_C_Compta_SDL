0bf778f2336f952b2853be4490e58b188b8f059d
Charles-Antoine <charto.bta@gmail.com>
2024-03-13 19:05:11 +0100

    fill object and delete object

--- main.c
+++ main.c
@@ -14,32 +14,50 @@
 
 
 // Définition des stacks
-Stack stack1 = {.x = 0, .y = 0, .w = STACKWIDTH, .h = STACKHEIGHT, .r = 0, .g=0, .b=255, .a=255};
-Stack stack2 = {.x = STACKWIDTH, .y = 0, .w = STACKWIDTH, .h = STACKHEIGHT, .r = 0, .g=255, .b=0, .a=255};
-Stack stack3 = {.x = 2 * STACKWIDTH, .y = 0, .w = STACKWIDTH, .h = STACKHEIGHT, .r = 255, .g=0, .b=0, .a=255};
-Stack stack4 = {.x = 0, .y = STACKHEIGHT, .w = STACKWIDTH, .h = STACKHEIGHT, .r = 255, .g=165, .b=0, .a=255};
-Stack stack5 = {.x = STACKWIDTH, .y = STACKHEIGHT, .w = STACKWIDTH, .h = STACKHEIGHT, .r = 128, .g=0, .b=128, .a=255};
-Stack stack6 = {.x = 2 * STACKWIDTH, .y = STACKHEIGHT, .w = STACKWIDTH, .h = STACKHEIGHT, .r = 255, .g=192, .b=203, .a=255};
+Stack stacks[6] = {
+    {.x = 0, .y = 0, .w = STACKWIDTH, .h = STACKHEIGHT, .r = 0, .g=0, .b=255, .a=255},
+    {.x = STACKWIDTH, .y = 0, .w = STACKWIDTH, .h = STACKHEIGHT, .r = 0, .g=255, .b=0, .a=255},
+    {.x = 2 * STACKWIDTH, .y = 0, .w = STACKWIDTH, .h = STACKHEIGHT, .r = 255, .g=0, .b=0, .a=255},
+    {.x = 0, .y = STACKHEIGHT, .w = STACKWIDTH, .h = STACKHEIGHT, .r = 255, .g=165, .b=0, .a=255},
+    {.x = STACKWIDTH, .y = STACKHEIGHT, .w = STACKWIDTH, .h = STACKHEIGHT, .r = 128, .g=0, .b=128, .a=255},
+    {.x = 2 * STACKWIDTH, .y = STACKHEIGHT, .w = STACKWIDTH, .h = STACKHEIGHT, .r = 255, .g=192, .b=203, .a=255}
+};
+
 
 void fillBackgroundStack(SDL_Renderer *renderer){
-    SDL_SetRenderDrawColor(renderer, stack1.r, stack1.g, stack1.b, stack1.a);
-    SDL_RenderFillRect(renderer, &stack1.rect);
+    SDL_SetRenderDrawColor(renderer, stacks[0].r, stacks[0].g, stacks[0].b, stacks[0].a);
+    SDL_RenderFillRect(renderer, &stacks[0].rect);
 
-    SDL_SetRenderDrawColor(renderer, stack2.r, stack2.g, stack2.b, stack2.a);
-    SDL_RenderFillRect(renderer, &stack2.rect);
+    SDL_SetRenderDrawColor(renderer, stacks[1].r, stacks[1].g, stacks[1].b, stacks[1].a);
+    SDL_RenderFillRect(renderer, &stacks[1].rect);
 
-    SDL_SetRenderDrawColor(renderer, stack3.r, stack3.g, stack3.b, stack3.a);
-    SDL_RenderFillRect(renderer, &stack3.rect);
+    SDL_SetRenderDrawColor(renderer, stacks[2].r, stacks[2].g, stacks[2].b, stacks[2].a);
+    SDL_RenderFillRect(renderer, &stacks[2].rect);
 
-    SDL_SetRenderDrawColor(renderer, stack4.r, stack4.g, stack4.b, stack4.a);
-    SDL_RenderFillRect(renderer, &stack4.rect);
+    SDL_SetRenderDrawColor(renderer, stacks[3].r, stacks[3].g, stacks[3].b, stacks[3].a);
+    SDL_RenderFillRect(renderer, &stacks[3].rect);
     
-    SDL_SetRenderDrawColor(renderer, stack5.r, stack5.g, stack5.b, stack5.a);
-    SDL_RenderFillRect(renderer, &stack5.rect);
+    SDL_SetRenderDrawColor(renderer, stacks[4].r, stacks[4].g, stacks[4].b, stacks[4].a);
+    SDL_RenderFillRect(renderer, &stacks[4].rect);
 
-    SDL_SetRenderDrawColor(renderer, stack6.r, stack6.g, stack6.b, stack6.a);
-    SDL_RenderFillRect(renderer, &stack6.rect);
+    SDL_SetRenderDrawColor(renderer, stacks[5].r, stacks[5].g, stacks[5].b, stacks[5].a);
+    SDL_RenderFillRect(renderer, &stacks[5].rect);
     
+}
+
+void fillObjectInStack(SDL_Renderer *renderer)
+{
+    for (int i = 0; i < 6; i++)
+    {
+        ObjectPile *pile = stacks[i].head;
+        while (stacks[i].head != NULL)
+        {
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+            SDL_RenderFillRect(renderer, &stacks[i].head->object->rect);
+            stacks[i].head = stacks[i].head->next;
+        }
+        stacks[i].head = pile;
+    }
 }
 
 
@@ -102,19 +120,35 @@ int main(int argc, char *argv[])
     SDL_Color textColorBlack = {0, 0, 0, 255};       // Black text
     SDL_Color textColorGray = {128, 128, 128, 255};  // Gray text
 
-    stack1.rect = (SDL_Rect){stack1.x, stack1.y, stack1.w, stack1.h};
-    stack2.rect = (SDL_Rect){stack2.x, stack2.y, stack2.w, stack2.h};
-    stack3.rect = (SDL_Rect){stack3.x, stack3.y, stack3.w, stack3.h};
-    stack4.rect = (SDL_Rect){stack4.x, stack4.y, stack4.w, stack4.h};
-    stack5.rect = (SDL_Rect){stack5.x, stack5.y, stack5.w, stack5.h};
-    stack6.rect = (SDL_Rect){stack6.x, stack6.y, stack6.w, stack6.h};
-
-
-
+    stacks[0].rect = (SDL_Rect){stacks[0].x, stacks[0].y, stacks[0].w, stacks[0].h};
+    stacks[1].rect = (SDL_Rect){stacks[1].x, stacks[1].y, stacks[1].w, stacks[1].h};
+    stacks[2].rect = (SDL_Rect){stacks[2].x, stacks[2].y, stacks[2].w, stacks[2].h};
+    stacks[3].rect = (SDL_Rect){stacks[3].x, stacks[3].y, stacks[3].w, stacks[3].h};
+    stacks[4].rect = (SDL_Rect){stacks[4].x, stacks[4].y, stacks[4].w, stacks[4].h};
+    stacks[5].rect = (SDL_Rect){stacks[5].x, stacks[5].y, stacks[5].w, stacks[5].h};
 #pragma endregion
     SDL_Event event;
     Bool running = TRUE;
     Mouse mouse;
+
+
+    AddObject(&stacks[0], *creat_Object("Pomme"));
+    AddObject(&stacks[0], *creat_Object("Poire"));
+    AddObject(&stacks[1], *creat_Object("Banane"));
+    AddObject(&stacks[0], *creat_Object("Orange"));
+
+    AddObject(&stacks[5], *creat_Object("Pomme"));
+    AddObject(&stacks[5], *creat_Object("Poire"));
+    AddObject(&stacks[5], *creat_Object("Banane"));
+    AddObject(&stacks[5], *creat_Object("Orange"));
+    AddObject(&stacks[5], *creat_Object("Pomme"));
+    AddObject(&stacks[5], *creat_Object("Poire"));
+    AddObject(&stacks[5], *creat_Object("Banane"));
+    AddObject(&stacks[5], *creat_Object("Orange"));
+
+    deleteObjectFromStack(&stacks[0], 0);
+
+    
 
     while (running)
     {
@@ -134,6 +168,7 @@ int main(int argc, char *argv[])
 
         // Dessiner la grille
         fillBackgroundStack(renderer);
+        fillObjectInStack(renderer);
 
         SDL_RenderPresent(renderer);
         SDL_Delay(1000/FPS);
--- tools_for_stock.c
+++ tools_for_stock.c
@@ -1,18 +1,135 @@
+int nbObjects = 0;
+
 void AddObject(Stack *stack, Object object)
 {
     ObjectPile *newPile = malloc(sizeof(ObjectPile));
-    object.rect = (SDL_Rect){stack->x + 10, stack->y + 5 + 10*stack->quantity, 30, 10};
+    if (stack->quantity < 5)
+    {
+        object.rect = (SDL_Rect){stack->x + 80, stack->y + 20 + (40 * stack->quantity), 100, 30};
+    }
+    else
+    {
+        object.rect = (SDL_Rect){stack->x + 200, stack->y + 20 + (40 * (stack->quantity - 5)), 100, 30};
+    }
     stack->quantity++;
     newPile->object = &object;
     newPile->next = stack->head;
     stack->head = newPile;
 }
 
-Object* creat_Object(char Name[50])
+Object *creat_Object(char Name[50])
 {
     Object *object = malloc(sizeof(Object));
-    object->ID = 0;
+    object->ID = nbObjects;
+    nbObjects++;
     strcpy(object->Name, Name);
     return object;
 }
 
+int deleteObjectFromStack(Stack *stack, int ID)
+{
+    Bool found = FALSE;
+    ObjectPile *pile = stack->head;
+    ObjectPile *prev = NULL;
+    Stack *stackModified = NULL;
+    while (stack->head != NULL)
+    {
+        if (stack->head->object->ID == ID)
+        {
+            if (prev == NULL)
+            {
+                stack->head = stack->head->next;
+            }
+            else
+            {
+                prev->next = stack->head->next;
+            }
+            free(stack->head);
+            found = TRUE;
+        }
+        prev = stack->head;
+        stack->head = stack->head->next;
+        if (found)
+            break;
+    }
+    stack->head = pile;
+    if (!found)
+        return 1;
+
+    ObjectPile *pile2 = stack->head;
+    int quantity = 0;
+    while (stack->head != NULL)
+    {
+        if (quantity < 5)
+        {
+            stack->head->object->rect = (SDL_Rect){stack->x + 80, stack->y + 20 + (40 * quantity), 100, 30};
+        }
+        else
+        {
+            stack->head->object->rect = (SDL_Rect){stack->x + 200, stack->y + 20 + (40 * (quantity - 5)), 100, 30};
+        }
+        
+        quantity++;
+        stack->head = stack->head->next;
+    }
+    stack->head = pile;
+
+    return 0;
+}
+
+int deleteObject(Stack stacks[], int ID)
+{
+    Bool found = FALSE;
+    Stack *stackModified = NULL;
+    for (int i = 0; i < 6; i++)
+    {
+        if (found)
+            break;
+        ObjectPile *pile = stacks[i].head;
+        ObjectPile *prev = NULL;
+        while (stacks[i].head != NULL)
+        {
+            if (stacks[i].head->object->ID == ID)
+            {
+                if (prev == NULL)
+                {
+                    stacks[i].head = stacks[i].head->next;
+                }
+                else
+                {
+                    prev->next = stacks[i].head->next;
+                }
+                free(stacks[i].head);
+                found = TRUE;
+                stackModified = &stacks[i];
+            }
+            prev = stacks[i].head;
+            stacks[i].head = stacks[i].head->next;
+            if (found)
+                break;
+        }
+        stacks[i].head = pile;
+    }
+
+    if (!found)
+        return 1;
+
+    ObjectPile *pile = stackModified->head;
+    int quantity = 0;
+    while (stackModified->head != NULL)
+    {
+        if (quantity < 5)
+        {
+            stackModified->head->object->rect = (SDL_Rect){stackModified->x + 80, stackModified->y + 20 + (40 * quantity), 100, 30};
+        }
+        else
+        {
+            stackModified->head->object->rect = (SDL_Rect){stackModified->x + 200, stackModified->y + 20 + (40 * (quantity - 5)), 100, 30};
+        }
+        
+        quantity++;
+        stackModified->head = stackModified->head->next;
+    }
+    stackModified->head = pile;
+    return 0;
+}
